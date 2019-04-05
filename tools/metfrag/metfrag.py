88021ecfe4eb7724b80c5e5008c10bec78f838d2
Julien Saint-Vanne <35449860+jsaintvanne@users.noreply.github.com>
2019-04-05 12:19:17 +0200

    Add filters and update tests (#7)
    * Modify script for new create msp
    
    * Add filter for preprocessing
    
    * Add thresholds
    
    * update test files
    
    * delete files
    
    * update file test

--- tools/metfrag/metfrag.py
+++ tools/metfrag/metfrag.py
@@ -2,6 +2,8 @@ import argparse
 import csv
 import os
 import sys
+from collections import defaultdict
+
 print(sys.version)
 
 parser = argparse.ArgumentParser()
@@ -12,29 +14,83 @@ parser.add_argument('--ppm')
 parser.add_argument('--ppm_frag')
 parser.add_argument('--fragmasstol')
 parser.add_argument('--polarity')
+parser.add_argument('--minMSMSpeaks')
 parser.add_argument('--results')
 parser.add_argument('--threads')
+parser.add_argument('--unconnectcompnd', action='store_true')
+parser.add_argument('--isotopefilter', action='store_true')
+parser.add_argument('--minelem')
+parser.add_argument('--maxelem')
+parser.add_argument('--subinclusion')
+parser.add_argument('--subexclusion')
+parser.add_argument('--eleminclusion')
+parser.add_argument('--elemexcluinclusion')
+parser.add_argument('--elemexclusion')
+parser.add_argument('--score_thrshld')
+parser.add_argument('--pctexplpeak_thrshld')
 
 args = parser.parse_args()
 print args
 
-os.makedirs("tmet")
+
+
+#Stock filters in dict (names have been choose to add them directly in MetFragPreProcessingCandidateFilter parameter)
+dct_filter = defaultdict(list)
+dct_filter['UnconnectedCompoundFilter'].append(args.unconnectcompnd)
+dct_filter['IsotopeFilter'].append(args.isotopefilter)
+dct_filter['MinimumElementsFilter'].append(args.minelem)
+dct_filter['MinimumElementsFilter'].append("FilterMinimumElements")
+dct_filter['MaximumElementsFilter'].append(args.maxelem)
+dct_filter['MaximumElementsFilter'].append("FilterMaximumElements")
+dct_filter['SmartsSubstructureInclusionFilter'].append(args.subinclusion)
+dct_filter['SmartsSubstructureInclusionFilter'].append("FilterSmartsInclusionList")
+dct_filter['SmartsSubstructureExclusionFilter'].append(args.subexclusion)
+dct_filter['SmartsSubstructureExclusionFilter'].append("FilterSmartsExclusionList")
+dct_filter['ElementInclusionFilter'].append(args.eleminclusion)
+dct_filter['ElementInclusionFilter'].append("FilterIncludedElements")
+dct_filter['ElementInclusionExclusiveFilter'].append(args.elemexcluinclusion)
+dct_filter['ElementInclusionExclusiveFilter'].append("FilterIncludedElements")
+dct_filter['ElementExclusionFilter'].append(args.elemexclusion)
+dct_filter['ElementExclusionFilter'].append("FilterExcludedElements")
+
+print "\nAll filters :\n"
+for key,val in dct_filter.items():
+    print key, "=>", val
+#Keep only used filters
+dct_keepf = defaultdict(list)
+for n in dct_filter:
+    if dct_filter[n][0] != '':
+        dct_keepf[n] = dct_filter[n]
+print "\nFilter(s) to use :\n"
+for key,val in dct_keepf.items():
+    print key, "=>", val
+print "\n"
+
+#Create repo to work
+if not os.path.exists("./tmet"):
+    os.makedirs("./tmet")
+else:
+    #Clean the repo if already exist
+    for filename in os.listdir("./tmet"):
+        os.remove("./tmet" + "/" + filename)
 
 with open(args.input,"r") as infile:
     numlines = 0
     for line in infile:
         line = line.strip()
         if numlines == 0:
-            if "NAME" in line:
-                featid = line.split("NAME: ")[1]
-            if "PRECURSORMZ" in line:
-                mz = float(line.split("PRECURSORMZ: ")[1])
+            if "CH$NAME:" in line:
+                featid = line.split("CH$NAME: ")[1]
+                featid = featid.split(" ")
+                featid = featid[5].split(":")[1] + "-" + featid[7].split(":")[1] + "-" + featid[3].split(":")[1]
+            if "MS$FOCUSED_ION:" in line:
+                mz = float(line.split("MS$FOCUSED_ION: PRECURSOR_M/Z ")[1])
                 if args.polarity=="pos":
                     mz2 = mz-1.007276
                 else:
                     mz2 = mz+1.007276
-            if "Num Peaks" in line:
-                numlines = int(line.split("Num Peaks: ")[1])
+            if "PK$NUM_PEAK:" in line:
+                numlines = int(line.split("PK$NUM_PEAK: ")[1])
                 linesread = 0
                 peaklist = []
         else:
@@ -45,7 +101,7 @@ with open(args.input,"r") as infile:
                     for p in peaklist:
                         outfile.write(p[0]+"\t"+p[1]+"\n")
                 #create commandline input
-                cmd_command = "PeakListPath=tmpspec.txt "
+                cmd_command = "PeakListPath=./tmpspec.txt "
                 if args.db_local != "None":
                     cmd_command += "MetFragDatabaseType=LocalCSV "
                     cmd_command += "LocalDatabasePath={0} ".format(args.db_local)
@@ -67,13 +123,40 @@ with open(args.input,"r") as infile:
                     cmd_command += "PrecursorIonMode=-1 "
                 cmd_command += "MetFragCandidateWriter=CSV " ## TSV not available
                 cmd_command += "NumberThreads={} ".format(args.threads)
-                # run Metfrag
-                print "metfrag {0}".format(cmd_command)
-                os.system("metfrag {0}".format(cmd_command))
+
+                #All pre processing filters
+                param = []
+                defparam = defaultdict(list)
+                for f in dct_keepf:
+                    if f == "UnconnectedCompoundFilter":
+                        if dct_keepf[f][0] == True:
+                            param .append("UnconnectedCompoundFilter")
+                    elif f == "IsotopeFilter":
+                        if dct_keepf[f][0] == True:
+                            param.append("IsotopeFilter")
+                    else:
+                        param.append(f)
+                        defparam[dct_keepf[f][1]].append(dct_keepf[f][0])
+
+                if param:
+                    cmd_command += "MetFragPreProcessingCandidateFilter={} ".format(','.join(param))
+                    for key in defparam:
+                        cmd_command += "{0}={1} ".format(str(key),str(defparam[key][0]))
+                
+
+                #Filter before process with a minimum number of MS/MS peaks
+                if linesread >= float(args.minMSMSpeaks):
+                    # run Metfrag
+                    print "metfrag {0}".format(cmd_command)
+                    os.system("metfrag {0}".format(cmd_command))
             else:
-                line = tuple(line.split("\t"))
-                linesread += 1
-                peaklist.append(line)
+                #One line not need between numpeak and peaklist
+                if not "PK$PEAK:" in line:
+                    line = tuple(line.split("\t"))
+                    #Keep only m/z and intensity, not relative intensity
+                    save_line = tuple(line[0].split("\t") + line[1].split("\t"))
+                    linesread += 1
+                    peaklist.append(save_line)
 
 
 #outputs might have different headers. Need to get a list of all the headers before we start merging the files
@@ -90,15 +173,12 @@ for fname in outfiles:
             c+=1
             if i==1:
                 break
-            
+
 # if no data rows (e.g. matches) then do not save an output and leave the program        
 if c==0:
     sys.exit()
 
-
-print headers
 headers = ['UID'] + sorted(list(set(headers)))
-print headers
 
 #merge outputs
 with open(args.results, 'a') as merged_outfile:
@@ -111,7 +191,27 @@ with open(args.results, 'a') as merged_outfile:
         with open("./tmet/"+fname) as infile:
             reader = csv.DictReader(infile, delimiter=',', quotechar='"')
             for line in reader:
-                line['UID'] = fileid
-                dwriter.writerow(line)
+                bewrite = True
+                for key, value in line.items():
+                    #Filter when no MS/MS peak matched
+                    if key == "ExplPeaks":
+                        if "NA" in value:
+                            bewrite = False
+                    #Filter with a score threshold
+                    elif key == "Score":
+                        if value <= args.score_thrshld:
+                            bewrite = False
+                    elif key == "NoExplPeaks":
+                        nbfindpeak = float(value)
+                    elif key == "NumberPeaksUsed":
+                        totpeaks = float(value)
+                #Filter with a relative number of peak matched
+                pctexplpeak = nbfindpeak / totpeaks * 100
+                if pctexplpeak < float(args.pctexplpeak_thrshld):
+                    bewrite = False
+                #Write the line if it pass all filters
+                if bewrite:
+                    line['UID'] = fileid
+                    dwriter.writerow(line)
 
 
