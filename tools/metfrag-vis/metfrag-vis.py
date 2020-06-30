c9eb25baeb6223d7609c425a7f53c13aec6ec1a1
tomnl <thomas.nigel.lawson@gmail.com>
2020-06-30 20:28:39 +0100

    update metfrag-vis to pass planemo tests

--- tools/metfrag-vis/metfrag-vis.py
+++ tools/metfrag-vis/metfrag-vis.py
@@ -33,6 +33,7 @@ parser.add_argument('-s', '--synonyms', dest='synonyms', action='store_true', re
 parser.add_argument('-c', '--classyfire', dest='classyfire', action='store_true', required=False,
                    help='Fetch compound classes from ClassyFire [disabled by default]')
 
+
 args = parser.parse_args()
 
 # Input CSV with MetFrag results
@@ -51,16 +52,19 @@ pubchem_synonyms_enabled = args.synonyms
 classyfire_classes_enabled = args.classyfire
 
 
-
 # ---------- cdk_inchi_to_svg ----------
 def cdk_inchi_to_svg(inchi):
-	JAVA_BINARY = '/usr/local/bin/java'
-	CDK_INCHI_TO_SVG_JAR = '/usr/local/bin/cdk-inchi-to-svg-0.0.1-SNAPSHOT-jar-with-dependencies.jar'
-	JAVA_CMD = str(JAVA_BINARY + ' ' + '-jar' + ' ' + CDK_INCHI_TO_SVG_JAR + ' ' + str('\'' + inchi + '\'') + ' ' + 'cdk-inchi-to-svg-output.svg')
-	
+	if "cdk-inchi-to-svg" in os.environ:
+		JAVA_CMD = 'cdk-inchi-to-svg' + ' ' + str('\'' + inchi + '\'') + ' ' + 'cdk-inchi-to-svg-output.svg'
+	else:
+		JAVA_BINARY = '/usr/local/bin/java'
+		CDK_INCHI_TO_SVG_JAR = '/usr/local/bin/cdk-inchi-to-svg-0.0.1-SNAPSHOT-jar-with-dependencies.jar'
+		JAVA_CMD = str(JAVA_BINARY + ' ' + '-jar' + ' ' + CDK_INCHI_TO_SVG_JAR + ' ' + str('\'' + inchi + '\'') + ' ' + 'cdk-inchi-to-svg-output.svg')
+
+
 	# Exec cdk-inchi-to-svg JAVA binary
 	exitcode = os.system(JAVA_CMD)
-	
+
 	# Check whether binary has successfully been run
 	if (exitcode == 0):
 		with open("cdk-inchi-to-svg-output.svg", "r") as svg_file:
@@ -114,7 +118,7 @@ def chebi_link(inchi):
 def fetch_pubchem_synonyms(inchi):
 	if not ('InChI=' in inchi):
 		return('&nbsp;')
-	
+
 	# Fetch CID from InChI
 	print('Retrieving PubChem CID from InChI...')
 	compound = pubchempy.get_compounds(identifier=inchi, namespace='inchi')
@@ -122,7 +126,7 @@ def fetch_pubchem_synonyms(inchi):
 	if (len(compound_cid) <= 1):
 		print(str('Warning. No match for InChI \"' + str(inchi) + '\".'))
 		return('&nbsp;')
-	
+
 	# Retrieve compound
 	print('Retrieving PubChem compound information...')
 	compound = pubchempy.Compound.from_cid(compound_cid)
@@ -138,42 +142,42 @@ def fetch_pubchem_synonyms(inchi):
 def fetch_classyfire_classes(inchi):
 	if not ('InChI=' in inchi):
 		return('&nbsp;')
-	
+
 	# Send POST request to ClassyFire
 	print('Sending request to ClassyFire...')
 	classyfire_url = 'http://classyfire.wishartlab.com/queries.json'
 	classyfire_post = str('{\"label\":\"metfrag\",\"query_input\":\"' + inchi + '\",\"query_type\":\"STRUCTURE\"}')
 	classyfire_headers = { 'Content-Type' : 'application/json' }
 	classyfire_request = requests.post(classyfire_url, data=classyfire_post, headers=classyfire_headers)
-	
+
 	# Only continue when request has been successfully sent
 	if (classyfire_request.status_code != 201):
 		print('Error! Could not send request to ClassyFire. \"', str(classyfire_request.status_code) + ': ' + str(classyfire_request.reason), '\". Skipping entry.')
 		return('&nbsp;')
-	
+
 	# Get ClassyFire Query ID
 	classyfire_request.json()
 	classyfire_query_id = classyfire_request.json()['id']
-	
+
 	# Query ClassyFire in max. 20 attempts
 	classyfire_request_loop = 0
 	while (classyfire_request_loop < 20):
 		print(str('Sending query ' + str(classyfire_query_id) + ' to ClassyFire...'))
 		time.sleep(10)
 		classyfire_query = requests.get(str('http://classyfire.wishartlab.com/queries/' + str(classyfire_query_id) + '.json'))
-		
+
 		if (classyfire_query.status_code == 200) and (classyfire_query.json()['classification_status'] == 'Done'):
 			classyfire_request_loop = 999
 			break
 		else:
 			classyfire_request_loop += 1
-	
+
 	if (classyfire_request_loop == 999):
 		# Direct parent
 		direct_parent_name = classyfire_query.json()['entities'][0]['direct_parent']['name']
 		direct_parent_url = classyfire_query.json()['entities'][0]['direct_parent']['url']
 		direct_parent = str('<a target="_blank" href="' + direct_parent_url + '">' + direct_parent_name + '</a>')
-		
+
 		# Alternative parents
 		alt_parents = []
 		for i in range(0, len(classyfire_query.json()['entities'][0]['alternative_parents'])):
@@ -181,13 +185,13 @@ def fetch_classyfire_classes(inchi):
 			alt_parent_url = classyfire_query.json()['entities'][0]['alternative_parents'][i]['url']
 			alt_parent = str('<a target="_blank" href="' + alt_parent_url + '">' + alt_parent_name + '</a>')
 			alt_parents.append(alt_parent)
-		
+
 		# Concat classes
 		classes = str('<b>' + direct_parent + '</b>, <br>' + str(', <br>'.join(alt_parents)))
 	else:
 		print('Warning. Timout sending query to ClassyFire. Skipping entry.')
 		classes = '&nbsp;'
-	
+
 	return(classes)
 
 
@@ -198,7 +202,7 @@ def plot_spectrum(spectrum, spectrum_explained, spectrum_explained_formulas):
 	plt.figure(figsize=[5.5, 4.4])
 	plt.xlabel('m/z')
 	plt.ylabel('intensity')
-	
+
 	# Plot spectrum
 	x = []
 	y = []
@@ -206,11 +210,11 @@ def plot_spectrum(spectrum, spectrum_explained, spectrum_explained_formulas):
 		t = i.split('_')
 		x.append(t[0])
 		y.append(t[1])
-	
+
 	for i in range(0, len(x)):
 		plt.plot([float(x[i]), float(x[i])], [0, float(y[i])], linewidth=1, color='black')
 		plt.plot(float(x[i]), float(y[i]), 'o', color='black', markersize=4)
-	
+
 	if not (spectrum_explained == 'NA') and not (spectrum_explained_formulas == 'NA'):
 		# Plot explained peaks
 		ex = []
@@ -219,11 +223,11 @@ def plot_spectrum(spectrum, spectrum_explained, spectrum_explained_formulas):
 			t = i.split('_')
 			ex.append(t[0])
 			ey.append(y[x.index(t[0])])
-		
+
 		for i in range(0, len(ex)):
 			plt.plot([float(ex[i]), float(ex[i])], [0, float(ey[i])], linewidth=3, color='#2b8126')
 			plt.plot(float(ex[i]), float(ey[i]), 'o', color='#2b8126', markersize=8)
-		
+
 		# Plot formulas on explained peaks
 		ex = []
 		ey = []
@@ -233,14 +237,14 @@ def plot_spectrum(spectrum, spectrum_explained, spectrum_explained_formulas):
 			ex.append(t[0])
 			ey.append(y[x.index(t[0])])
 			ez.append(t[1])
-		
+
 		for i in range(0, len(ex)):
 			plt.text(float(ex[i]), float(ey[i])+1000, ez[i], color='#2b8126', fontsize=8, horizontalalignment='center', verticalalignment='bottom')
-	
+
 	# Save SVG
 	plt.savefig("metfrag-vis-spectrum.svg", format="svg", transparent=True)
 	plt.close()
-	
+
 	# Import SVG
 	with open("metfrag-vis-spectrum.svg", "r") as svg_file:
 		svg_string = []
@@ -290,16 +294,24 @@ metfrag_html.write('<body>\n')
 
 # Read input csv file
 with open(input_tsv, "r") as metfrag_file:
-	metfrag_results = csv.DictReader(metfrag_file, delimiter='\t')
-	
+	dialect = csv.Sniffer().sniff(metfrag_file.read(1024))
+	metfrag_file.seek(0)
+
+	metfrag_results = csv.DictReader(metfrag_file, delimiter=dialect.delimiter)
 	# Parse each line
 	line_count = 0
 	compound = ""
 	candidates = 0
 	for row in metfrag_results:
+
 		# Start new document
 		if (line_count == 0):
-			with open("/usr/local/share/metfrag/metfrag_logo.png", "rb") as png_file:
+			if os.path.join(os.path.dirname(os.path.abspath(__file__)), 'metfrag_logo.png'):
+				logo_pth = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'metfrag_logo.png')
+			else:
+				logo_pth = '/usr/local/share/metfrag/metfrag_logo.png'
+
+			with open(logo_pth, "rb") as png_file:
 				png_encoded = base64.b64encode(png_file.read())
 			metfrag_html.write(str('\n<h1><img style="vertical-align:bottom" src="data:image/png;base64,' + png_encoded.decode('utf-8') + '" alt="metfrag-logo" width="150"></img><text style="line-height:2.0">&nbsp;&nbsp;results</text></h1>\n'))
 		else:
@@ -323,10 +335,10 @@ with open(input_tsv, "r") as metfrag_file:
 				precursor_rt = row["retention_time"]
 				precursor_mz = row["precursor_mz"]
 				precursor_type = row["precursor_type"]
-				
+
 				if (line_count > 1):
 					metfrag_html.write(str('</table>\n'))
-				
+
 				metfrag_html.write(str('\n' + '<h2>' + identifier + '</h2>\n'))
 				metfrag_html.write(str('<p><b>Precursor Type:</b> ' + str(precursor_type) + '<br>'))
 				metfrag_html.write(str('<b>Precursor Mass:</b> ' + str(round(float(precursor_mz), 4)) + '<br>'))
@@ -349,7 +361,7 @@ with open(input_tsv, "r") as metfrag_file:
 										      + '<td>' + '<b>External Links</b>' + '</td>'
 										      + '<td class="tdmax">' + '<b>InChI</b>' + '</td>'
 										      + '</tr>\n'))
-			
+
 			# Compound candidate
 			if (candidates < max_candidates):
 				# Column variables
@@ -369,33 +381,33 @@ with open(input_tsv, "r") as metfrag_file:
 				spectrum_explained = row["ExplPeaks"]
 				spectrum_explained_formulas = row["FormulasOfExplPeaks"]
 				identifier = row["Identifier"]
-				
+
 				# PubChem Synonyms
 				if (pubchem_synonyms_enabled == True):
 					pubchem_synonyms = fetch_pubchem_synonyms(inchi)
 				else:
 					pubchem_synonyms = '&nbsp;'
-				
+
 				# Compound Classes
 				if (classyfire_classes_enabled == True):
 					compound_classes = fetch_classyfire_classes(inchi)
 				else:
 					compound_classes = '&nbsp;'
-				
+
 				# Draw Spectrum
 				spectrum = re.sub(' .*', '', re.sub('.*PeakListString=', '', row["MetFragCLIString"]))
 				spectrum_string = plot_spectrum(spectrum, spectrum_explained, spectrum_explained_formulas)
-				
+
 				# Draw SVG
 				svg_string = cdk_inchi_to_svg(str(inchi))
-				
+
 				# External links
 				external_links = str('<a target="_blank" href="' + pubchem_link(compound_name) + '">PubChem</a>' + ', ' +
 				                     '<a target="_blank" href="' + kegg_link(compound_name) + '">KEGG</a>' + ', ' +
 				                     '<a target="_blank" href="' + hmdb_link(compound_name) + '">HMDB</a>' + ', ' +
 				                     '<a target="_blank" href="' + biocyc_link(compound_name) + '">BioCyc</a>' + ', ' +
 				                     '<a target="_blank" href="' + chebi_link(inchi) + '">ChEBI</a>')
-				
+
 				# MetFragWeb
 				FragmentPeakMatchAbsoluteMassDeviation = str('' + re.sub(' .*', '', re.sub('.*FragmentPeakMatchAbsoluteMassDeviation=', 'FragmentPeakMatchAbsoluteMassDeviation=', row["MetFragCLIString"])))
 				FragmentPeakMatchRelativeMassDeviation = str('' + re.sub(' .*', '', re.sub('.*FragmentPeakMatchRelativeMassDeviation=', 'FragmentPeakMatchRelativeMassDeviation=', row["MetFragCLIString"])))
@@ -406,8 +418,8 @@ with open(input_tsv, "r") as metfrag_file:
 				PrecursorIonMode = str('' + re.sub(' .*', '', re.sub('.*PrecursorIonMode=', 'PrecursorIonMode=', row["MetFragCLIString"])))
 				PeakList = str('' + re.sub(' .*', '', re.sub('.*PeakListString=', 'PeakList=', row["MetFragCLIString"])))
 				MetFragDatabaseType = str('' + re.sub(' .*', '', re.sub('.*MetFragDatabaseType=', 'MetFragDatabaseType=', row["MetFragCLIString"])))
-				
-				metfrag_web = str('https://msbi.ipb-halle.de/MetFrag/landing.xhtml?' + 
+
+				metfrag_web = str('https://msbi.ipb-halle.de/MetFrag/landing.xhtml?' +
 				                  FragmentPeakMatchAbsoluteMassDeviation + '&' +
 				                  FragmentPeakMatchRelativeMassDeviation + '&' +
 				                  DatabaseSearchRelativeMassDeviation + '&' +
@@ -417,7 +429,7 @@ with open(input_tsv, "r") as metfrag_file:
 				                  PrecursorIonMode + '&' +
 				                  PeakList + '&' +
 				                  MetFragDatabaseType)
-				
+
 				# Write html code
 				metfrag_html.write(str('<tr style="vertical-align:center">'
 				                              + '<td class="tdmax">' + spectrum_string + '</td>'
@@ -436,10 +448,10 @@ with open(input_tsv, "r") as metfrag_file:
 											  + '<td>' + external_links + '</td>'
 											  + '<td class="tdmax">' + inchi + '</td>'
 											  + '</tr>\n'))
-		
+
 		line_count += 1
 		candidates += 1
-	
+
 	# Finish candidate list
 	metfrag_html.write(str('</table>\n'))
 
